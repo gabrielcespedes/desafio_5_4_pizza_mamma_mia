1915ae062a45ef32030c68637dd7f5b7c946c615
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-03-21 22:32:25 -0300

    version estable

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,15 @@
+import imagen from '../delicious-pizza.jpg';
+
 const Home = () => {
     return(
-        <p>Home</p>
+        <div style={{backgroundImage: `url(${imagen})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}} className="bg-dark text-center hero">
+            <div className="container">
+                <h1 className="text-light">¡Pizzería Mamma Mia!</h1>
+                <p className="text-light">Tenemos las mejores pizzas que podrás encontrar!</p>
+                <hr className="text-light fs-4"></hr>
+            </div>
+            
+        </div>
     )
 }
 
