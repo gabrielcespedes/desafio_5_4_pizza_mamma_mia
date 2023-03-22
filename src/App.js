04d67a908c14b985697625a3c39dea7f92f21125
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-03-21 21:02:53 -0300

    version estable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  
+  const endpoint = "/pizzas.json";
+  const [pizza, setPizzas] = useState([]);
+  const estadoCompartido = {pizza, setPizzas};
+
+  useEffect(() => {
+    dataPizzas();
+  }, [])
+
+  const dataPizzas = async () => {
+    const responseData = await fetch(endpoint);
+    const dataPizzas = await responseData.json();
+    setPizzas([...dataPizzas]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>      
+          Learn React    
     </div>
   );
 }
